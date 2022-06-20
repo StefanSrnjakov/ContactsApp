9b00cdcf1473296e205db0de57141d9fca2dc8bb
Stefan Srnjakov <stefan.srnjakov@student.um.si>
2022-06-20 13:24:43 +0200

    konec

--- frontend/src/components/AddNewContact.js
+++ frontend/src/components/AddNewContact.js
@@ -66,7 +66,7 @@ function AddNewContact() {
                     <label>Telephone number</label>
                     <input type={"text"} className={"form-control"} placeholder={"Enter number"}
                            value={phoneNumber}
-                           pattern={"^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$"}
+                           pattern={"^((?:\\+|00)[17](?: |\\-)?|(?:\\+|00)[1-9]\\d{0,2}(?: |\\-)?|(?:\\+|00)1\\-\\d{3}(?: |\\-)?)?(0\\d|\\([0-9]{3}\\)|[1-9]{0,3})(?:((?: |\\-)[0-9]{2}){4}|((?:[0-9]{2}){4})|((?: |\\-)[0-9]{3}(?: |\\-)[0-9]{4})|([0-9]{7}))$"}
                            onChange={e => setPhoneNumber(e.target.value)}/>
 
                 </div>
